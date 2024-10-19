79f29127ac220ab7289a7041c69c5b16d326528a
mohamedgomaa20 <20monagy@gmail.com>
2024-10-19 09:57:14 +0300

    first commit

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:provider/provider.dart';
 
 import '../../layout.dart';
@@ -23,179 +24,185 @@ final formKey = GlobalKey<FormState>();
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailCon = TextEditingController();
   TextEditingController passCon = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // const LogoApp(),
-              const Gap(50),
-              Image.asset(
-                "assets/te2.png",
-                height: 150,
-                color: kPrimaryColor,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Welcome Back",
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              // Text(
-              //   "Material Chat App With Mohamed Gomaa",
-              //   style: Theme.of(context).textTheme.bodyLarge,
-              // ),
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // const LogoApp(),
+                const Gap(50),
+                Image.asset(
+                  "assets/te2.png",
+                  height: 150,
+                  color: kPrimaryColor,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Welcome Back",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                // Text(
+                //   "Material Chat App With Mohamed Gomaa",
+                //   style: Theme.of(context).textTheme.bodyLarge,
+                // ),
 
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    CustomField(
-                      controller: emailCon,
-                      lable: "Email",
-                      icon: Iconsax.direct,
-                    ),
-                    CustomField(
-                      controller: passCon,
-                      lable: "Password",
-                      icon: Iconsax.password_check,
-                      isPass: true,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      children: [
-                        const Spacer(),
-                        GestureDetector(
-                          child: const Text("Forget Password?"),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ForgetScreen(),
-                                ));
-                          },
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (formKey.currentState!.validate()) {
-                          signIn(email: emailCon.text, password: passCon.text);
-                          // await FirebaseAuth.instance
-                          //     .signInWithEmailAndPassword(
-                          //         email: emailCon.text, password: passCon.text)
-                          //     .then(
-                          //   (value) {
-                          //     print("---------- Login Done -------------");
-                          //   },
-                          // ).onError(
-                          //   (error, stackTrace) {
-                          //     ScaffoldMessenger.of(context).showSnackBar(
-                          //       SnackBar(content: Text(error.toString())),
-                          //     );
-                          //   },
-                          // );
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                        backgroundColor: kPrimaryColor,
-                        padding: const EdgeInsets.all(16),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      CustomField(
+                        controller: emailCon,
+                        lable: "Email",
+                        icon: Iconsax.direct,
                       ),
-                      child: Center(
-                        child: Text(
-                          "Login".toUpperCase(),
-                          style: const TextStyle(color: Colors.black),
-                        ),
+                      CustomField(
+                        controller: passCon,
+                        lable: "Password",
+                        icon: Iconsax.password_check,
+                        isPass: true,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    // ElevatedButton(
-                    //   onPressed: () async {
-                    //     if (formKey.currentState!.validate()) {
-                    //       // Sign out any currently logged-in user before creating a new one.
-                    //       await FirebaseAuth.instance.signOut();
-                    //
-                    //       await FirebaseAuth.instance
-                    //           .createUserWithEmailAndPassword(
-                    //           email: emailCon.text, password: passCon.text)
-                    //           .then((value) {
-                    //         emailCon.clear();
-                    //         passCon.clear();
-                    //       }).onError(
-                    //             (error, stackTrace) {
-                    //           ScaffoldMessenger.of(context).showSnackBar(
-                    //             SnackBar(content: Text(error.toString())),
-                    //           );
-                    //         },
-                    //       );
-                    //     }
-                    //   },
-                    //   style: ElevatedButton.styleFrom(
-                    //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                    //     backgroundColor: kPrimaryColor,
-                    //     padding: const EdgeInsets.all(16),
-                    //   ),
-                    //   child: Center(
-                    //     child: Text(
-                    //       "Create Account".toUpperCase(),
-                    //       style: const TextStyle(color: Colors.black),
-                    //     ),
-                    //   ),
-                    // ),
-
-                    OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12)),
-                          padding: const EdgeInsets.all(16),
-                        ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          GestureDetector(
+                            child: const Text("Forget Password?"),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ForgetScreen(),
+                                  ));
+                            },
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      ElevatedButton(
                         onPressed: () async {
                           if (formKey.currentState!.validate()) {
-                            await FirebaseAuth.instance
-                                .createUserWithEmailAndPassword(
-                                    email: emailCon.text,
-                                    password: passCon.text)
-                                .then((value) {
-                              emailCon.clear();
-                              passCon.clear();
-                            }).onError(
-                              (error, stackTrace) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text(error.toString())),
-                                );
-                              },
-                            );
+                            signIn(
+                                email: emailCon.text, password: passCon.text);
+                            // await FirebaseAuth.instance
+                            //     .signInWithEmailAndPassword(
+                            //         email: emailCon.text, password: passCon.text)
+                            //     .then(
+                            //   (value) {
+                            //     print("---------- Login Done -------------");
+                            //   },
+                            // ).onError(
+                            //   (error, stackTrace) {
+                            //     ScaffoldMessenger.of(context).showSnackBar(
+                            //       SnackBar(content: Text(error.toString())),
+                            //     );
+                            //   },
+                            // );
                           }
                         },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          backgroundColor: kPrimaryColor,
+                          padding: const EdgeInsets.all(16),
+                        ),
                         child: Center(
                           child: Text(
-                            "Create Account".toUpperCase(),
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.onBackground,
-                            ),
+                            "Login".toUpperCase(),
+                            style: const TextStyle(color: Colors.black),
                           ),
-                        )),
-                  ],
-                ),
-              )
-            ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      // ElevatedButton(
+                      //   onPressed: () async {
+                      //     if (formKey.currentState!.validate()) {
+                      //       // Sign out any currently logged-in user before creating a new one.
+                      //       await FirebaseAuth.instance.signOut();
+                      //
+                      //       await FirebaseAuth.instance
+                      //           .createUserWithEmailAndPassword(
+                      //           email: emailCon.text, password: passCon.text)
+                      //           .then((value) {
+                      //         emailCon.clear();
+                      //         passCon.clear();
+                      //       }).onError(
+                      //             (error, stackTrace) {
+                      //           ScaffoldMessenger.of(context).showSnackBar(
+                      //             SnackBar(content: Text(error.toString())),
+                      //           );
+                      //         },
+                      //       );
+                      //     }
+                      //   },
+                      //   style: ElevatedButton.styleFrom(
+                      //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                      //     backgroundColor: kPrimaryColor,
+                      //     padding: const EdgeInsets.all(16),
+                      //   ),
+                      //   child: Center(
+                      //     child: Text(
+                      //       "Create Account".toUpperCase(),
+                      //       style: const TextStyle(color: Colors.black),
+                      //     ),
+                      //   ),
+                      // ),
+
+                      OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                            padding: const EdgeInsets.all(16),
+                          ),
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: emailCon.text,
+                                      password: passCon.text)
+                                  .then((value) {
+                                emailCon.clear();
+                                passCon.clear();
+                              }).onError(
+                                (error, stackTrace) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text(error.toString())),
+                                  );
+                                },
+                              );
+                            }
+                          },
+                          child: Center(
+                            child: Text(
+                              "Create Account".toUpperCase(),
+                              style: TextStyle(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                              ),
+                            ),
+                          )),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -204,16 +211,30 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Future signIn({required String email, required String password}) async {
     try {
-      UserCredential userCredential =
+      setState(() {
+        isLoading = true;
+      });
+      final userCredential =
           await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => LayoutApp()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('No user found for that email.')));
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Wrong password provided for that user.')));
+      }
     } catch (e) {
       print("Failed to log in: $e");
     }
+    setState(() {
+      isLoading = false;
+    });
   }
 
 // Future<void> createAccount(String email, String password, String name) async {
